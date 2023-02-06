85126d5ee9c3cb2118515f4eae7e5f5b999a6728
Mubasshir Ahmed Arnab <111133967+mubasshirarnab@users.noreply.github.com>
2023-02-06 19:13:46 +0600

    Update Pattern.java
    1 2 3
    1 2 3
    1 2 3

--- Pattern.java
+++ Pattern.java
@@ -6,6 +6,7 @@ public class Pattern{
         int n = sc.nextInt();
 
         for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
                 System.out.print(j);
             }
             System.out.println("");
