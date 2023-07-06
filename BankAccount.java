332dc20b790576188cdd72b0cb39a87062efe03f
Mubasshir Ahmed Arnab <111133967+mubasshirarnab@users.noreply.github.com>
2023-07-07 00:03:27 +0600

    Add files via upload
    Create a Banking Application where a user can create new record, deposit 
    money, withdraw money and check the balance. [ Note: Must use private 
    members and getter and setter methods]

--- BankAccount.java
+++ BankAccount.java
@@ -1,64 +1,63 @@
-package LabManual;
-
-import java.util.Scanner;
-
 public class BankAccount {
-    public String name;
-    public String accountID;
-    public double balance;
+    private int accountNumber;
+    private double balance;
 
-    public void deposit(double a){
-        balance = balance + a;
-        System.out.println("Balance after Deposit: " + balance );
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
     }
 
-    public void withdraw(double b){
-        if(b<balance){
-            balance-=b;
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public BankAccount(int accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful. New balance: " + balance);
+        } else {
+            System.out.println("Invalid amount.");
         }
-        System.out.println("Balance after Withdraw: " + balance);
     }
 
-    public void Balance(){
-        System.out.println("Current Balance is: " + balance);
+    public void withdraw(double amount) {
+        if (amount > 0) {
+            if (balance >= amount) {
+                balance -= amount;
+                System.out.println("Withdrawal successful. New balance: " + balance);
+            } else {
+                System.out.println("Insufficient balance.");
+            }
+        } else {
+            System.out.println("Invalid amount.");
+        }
     }
-
-   /* public void display(){
-        deposit();
-        withdraw();
-        Balance();
-    } */
 }
 
-class Person{
+class Main1 {
     public static void main(String[] args) {
-        BankAccount a1 = new BankAccount();
-        BankAccount a2 = new BankAccount();
+        BankAccount account = new BankAccount(123456789, 1000.0);
 
-        Scanner sc = new Scanner(System.in);
+        System.out.println("Account Number: " + account.getAccountNumber());
+        System.out.println("Balance: " + account.getBalance());
 
-        a1.name = sc.nextLine();
-        a1.accountID = sc.nextLine();
-        a1.balance = sc.nextDouble();
+        account.deposit(500.0);
+        account.withdraw(200.0);
 
-        a2.name = sc.nextLine();
-        a2.accountID = sc.nextLine();
-        sc.nextLine();
-        a2.balance = sc.nextDouble();
-
-        System.out.println("First person's information: ");
-        System.out.println(a1.name);
-        System.out.println("Balance: " + a1.balance);
-        a1.deposit(2000);
-        a1.withdraw(1000);
-        a1.Balance();
-
-
-        System.out.println("Second person's information: ");
-        System.out.println(a2.name);
-        System.out.println("Balance: " + a2.balance);
-        a2.deposit(3000);
-        a2.withdraw(1500);
-        a2.Balance();
+        System.out.println("Balance: " + account.getBalance());
     }
 }
+
+
